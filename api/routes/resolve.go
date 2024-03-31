96985c2b7b2e77e0faf682423730e86f9d115a46
Shakezidin <sinuzidin@gmail.com>
2024-03-31 13:59:10 +0530

    getting done

--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -7,5 +7,24 @@ import (
 )
 
 func ResolveURL(c *fiber.Ctx) error {
+	url := c.Params("url")
 
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	value, err := r.Get(database.Ctx, url).Result()
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "short not found in the database",
+		})
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "cannot connect to DB",
+		})
+	}
+	rInr := database.CreateClient(1)
+	defer rInr.Close()
+
+	rInr.Incr(database.Ctx, "counter")
+	return c.Redirect(value, 301)
 }
