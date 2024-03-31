6bd1c2a2d367b52a04322f170c66b50e413fc141
Shakezidin <sinuzidin@gmail.com>
2024-03-31 13:58:18 +0530

    initial commit

---
+++ api/routes/resolve.go
@@ -0,0 +1,11 @@
+package routes
+
+import (
+	"github.com/go-redis/redis/v8"
+	"github.com/gofiber/fiber/v2"
+	"github.com/shakezidhin/urlShortner/database"
+)
+
+func ResolveURL(c *fiber.Ctx) error {
+
+}
---
+++ api/routes/shorten.go
@@ -0,0 +1,32 @@
+package routes
+
+import (
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	"github.com/go-redis/redis/v8"
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+	"github.com/shakezidhin/urlShortner/database"
+	"github.com/shakezidhin/urlShortner/helpers"
+)
+
+type request struct {
+	URL         string        `json:"url"`
+	CustomShort string        `json:"short"`
+	Expiry      time.Duration `json:"expiry"`
+}
+
+type response struct {
+	URL            string        `json:"url"`
+	CustomShort    string        `json:"short"`
+	Expiry         time.Duration `json:"expiry"`
+	XRateRemaining int           `json:"rate_limie"`
+	XRateLimitRest time.Duration `json:"rate_limie_reset"`
+}
+
+func ShortenURL(c *fiber.Ctx) error {
+	
+}
